75b980de36fd697a0084a25f97dbdea2538390fe
Aaron Lewis <CaledoniaProject@users.noreply.github.com>
2018-01-27 13:35:58 +0800

    Add files via upload

---
+++ build-mimikatz.ps1
@@ -0,0 +1,32 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$build_dir = $env:temp + "\mimikatz-master"
+$zip_path  = $env:temp + "\mimikatz-master.zip"
+
+Write-Host "[-] Downloading from github"
+$web = New-Object Net.Webclient;
+$web.DownloadFile("https://github.com/gentilkiwi/mimikatz/archive/master.zip", $zip_path);
+
+# 解压缩
+Write-Host "[-] Expanding zip"
+if (Test-Path -Path $build_dir)
+{
+    cmd /c del /Q /F /S "$build_dir" 
+    cmd /c rmdir "$build_dir" /q /s
+}
+# Remove-Item –Path "$build_dir" -Recurse -Force 
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zip_path, "$build_dir\..")
+
+# 修正编译脚本
+Write-Host "[-] Correcting vcxproj files"
+Get-ChildItem "$build_dir\*\*.vcxproj" | ForEach-Object -Process {
+    Write-Host " - " $_
+    (Get-Content $_) -Replace '<PreprocessorDefinitions>', '<PreprocessorDefinitions>UNICODE;' -Replace '<TreatWarningAsError>true</TreatWarningAsError>', '<TreatWarningAsError>false</TreatWarningAsError>' | Set-Content $_
+}
+
+# 开始编译
+Write-Host "[-] Start compilation"
+cd $build_dir
+cmd /c 'call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" && msbuild /p:PlatformToolset=v110 /p:Platform=x64'
+cmd /c 'call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" && msbuild /p:PlatformToolset=v110'
+
