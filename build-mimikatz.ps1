6eea844fc6f47d46314b3fb5cdf40d0cee3f23ee
Aaron Lewis <CaledoniaProject@users.noreply.github.com>
2018-01-27 13:42:02 +0800

    Update build-mimikatz.ps1

--- build-mimikatz.ps1
+++ build-mimikatz.ps1
@@ -2,6 +2,7 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $build_dir = $env:temp + "\mimikatz-master"
 $zip_path  = $env:temp + "\mimikatz-master.zip"
+$out_dir   = [Environment]::GetFolderPath("Desktop")
 
 Write-Host "[-] Downloading from github"
 $web = New-Object Net.Webclient;
@@ -29,4 +30,8 @@ Write-Host "[-] Start compilation"
 cd $build_dir
 cmd /c 'call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" && msbuild /p:PlatformToolset=v110 /p:Platform=x64'
 cmd /c 'call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" && msbuild /p:PlatformToolset=v110'
+
+Write-Host "[-] Copying files to $out_dir"
+Copy-Item "$build_dir\Win32\mimikatz.exe" -Destination "$out_dir\mimikatz32.exe"
+Copy-Item "$build_dir\x64\mimikatz.exe" -Destination "$out_dir\mimikatz64.exe"
 
